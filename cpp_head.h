3d19db3dba3ce7a44dda19e9a8d1ed223b92f25a
Mr.InkCandle <59001047+MrInkCandle@users.noreply.github.com>
2024-04-16 18:33:26 +0800

    Update and rename C Head to cpp_head.h

--- cpp_head.h
+++ cpp_head.h
@@ -1,3 +1,9 @@
+#include<iostream>    // IO
+#include<climits>     // Including common type size
+#include<string>      // Cpp string
+
+
+{ // Old
 #define TRUE 1
 #define FALSE 0
 #define ERROR -1
@@ -5,3 +11,4 @@ typedef unsigned char BYTE;
 typedef unsigned short WORD;
 typedef unsigned long DWORD;
 typedef unsigned double QWORD;
+}
