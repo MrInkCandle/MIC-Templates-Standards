a20bb0257bfb724455d09bea3caa3f762549804b
Mr.InkCandle <59001047+MrInkCandle@users.noreply.github.com>
2024-06-19 00:02:48 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,3 @@
-int main(int argc, char const* argv[], char* envp[]) {
+int main(int argc, char const* argv[], char const* envp[]) {
     return EXIT_SUCCESS;    // EXIT_SUCCESS and EXIT_FAILURE are from #include<cstdlib>
 }
