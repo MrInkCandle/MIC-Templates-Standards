7e423437144f4f2dbcc0fdf81d138e8cf7567027
Mr.InkCandle <59001047+MrInkCandle@users.noreply.github.com>
2024-06-18 23:55:24 +0800

    Update main.cpp to improve parameter
    Improve parameter

--- main.cpp
+++ main.cpp
@@ -1,3 +1,3 @@
-int main(int argc, char const* argv[]) {
+int main(int argc, char const* argv[], char* envp[]) {
     return EXIT_SUCCESS;    // EXIT_SUCCESS and EXIT_FAILURE are from #include<cstdlib>
 }
